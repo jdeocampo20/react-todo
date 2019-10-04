38194cce52c71d9a91cb8860f6701d43109bb5c2
James De Ocampo <jdeocampo20@gmail.com>
2019-10-04 22:19:16 +0100

    Removed unused COntainer import

--- src/Components/TodoContainer/TodoItem/TodoItem.js
+++ src/Components/TodoContainer/TodoItem/TodoItem.js
@@ -3,7 +3,6 @@ import Checkbox from '@material-ui/core/Checkbox';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import Icon from '@material-ui/core/Icon';
-import Container from '@material-ui/core/Container';
 
 import './TodoItem.scss';
 
