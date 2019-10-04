3067c9e3e2d87066c40052505c378a2ba6742503
James De Ocampo <jdeocampo20@gmail.com>
2019-10-04 22:18:58 +0100

    Changed TodoForm to a function instead of a class

--- src/Components/TodoContainer/TodoForm/TodoForm.js
+++ src/Components/TodoContainer/TodoForm/TodoForm.js
@@ -1,36 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Icon from '@material-ui/core/Icon';
 
 import './TodoForm.scss';
 
-class TodoForm extends React.Component {
-    state = { todo: '' };
-    handleSubmit = event => {
+const TodoForm = props => {
+    const [todo, setTodo] = useState('');
+    const handleSubmit = event => {
         event.preventDefault();
-        this.props.onSubmit(this.state.todo);
-        this.setState({ todo: '' });
+        props.onSubmit(todo);
+        setTodo('');
     };
 
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit} className='todo-form'>
-                <TextField
-                    type='text'
-                    variant='outlined'
-                    className='add-todo-field'
-                    value={this.state.todo}
-                    onChange={event =>
-                        this.setState({ todo: event.target.value })
-                    }
-                />
-                <Button type='submit' variant='contained' color='secondary'>
-                    <Icon>add</Icon>
-                </Button>
-            </form>
-        );
-    }
-}
+    return (
+        <form onSubmit={handleSubmit} className='todo-form'>
+            <TextField
+                type='text'
+                variant='outlined'
+                className='add-todo-field'
+                value={todo}
+                onChange={event => setTodo(event.target.value)}
+            />
+            <Button type='submit' variant='contained' color='secondary'>
+                <Icon>add</Icon>
+            </Button>
+        </form>
+    );
+};
 
 export default TodoForm;
