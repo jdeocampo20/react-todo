cfca9eb71bc9e64c58b30c5fcc9c21d6e3f13d67
James De Ocampo <jdeocampo20@gmail.com>
2019-10-21 23:50:47 +0100

    Made whole row clickable and fixed up stylings

--- src/Components/TodoContainer/TodoItem/TodoItem.js
+++ src/Components/TodoContainer/TodoItem/TodoItem.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Checkbox from '@material-ui/core/Checkbox';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
@@ -31,42 +31,38 @@ const TodoItem = props => {
         updatedTask.taskId = props.id;
         props.updateTaskDetails(updatedTask);
         setOpen(false);
-    }
+    };
 
     return (
-        <div className='todo-item'>
-            <div className='todo-details'>
+        <div className='flex-row-space-between'>
+            <div className='flex-row-center'>
                 <Checkbox
                     type='checkbox'
                     onClick={toggleTask}
                     value={props.complete}
                 ></Checkbox>
-                <Typography
-                    variant='h5'
-                    display='inline'
-                    style={{ textDecoration: props.complete && 'line-through' }}
-                >
-                    {props.value}
-                </Typography>
-            </div>
-            <div className='todo-details'>
-                <Typography
+                <div
+                    className='flex-row-space-between editable-row'
                     onClick={handleClickOpen}
-                    variant='subtitle2'
-                    display='inline'
                     style={{ textDecoration: props.complete && 'line-through' }}
                 >
-                    {moment(props.dueDate).format('ddd MMM DD')}
-                </Typography>
-                <IconButton
-                    aria-label='delete'
-                    onClick={deleteTask}
-                >
-                    <Icon>delete</Icon>
-                </IconButton>
+                    <span>
+                        <Typography variant='h5' display='inline'>
+                            {props.value}
+                        </Typography>
+                    </span>
+                    <span>
+                        <Typography variant='subtitle2' display='inline'>
+                            {moment(props.dueDate).format('ddd MMM DD')}
+                        </Typography>
+                    </span>
+                </div>
             </div>
-            <TodoFormDialog 
-                open={open} 
+            <IconButton aria-label='delete' onClick={deleteTask}>
+                <Icon>delete</Icon>
+            </IconButton>
+            <TodoFormDialog
+                open={open}
                 onClose={handleClose}
                 taskName={props.value}
                 dueDate={props.dueDate}
